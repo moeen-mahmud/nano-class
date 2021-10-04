6954dc1036a53840a114139877417a2c5a218d20
Moeen Mahmud <write2fahim007@gmail.com>
2021-10-04 16:43:47 +0600

    added accordion

--- src/components/Instructor/Instructor.js
+++ src/components/Instructor/Instructor.js
@@ -1,4 +1,11 @@
 import React from "react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+} from "@reach/accordion";
+import "@reach/accordion/styles.css";
 
 const Instructor = (props) => {
   const { instructor, instructorThumb, instructorAbout } = props.instructor;
@@ -13,9 +20,21 @@ const Instructor = (props) => {
       <h1 className="text-2xl font-semibold text-indigo-600 my-4">
         {instructor}
       </h1>
-      <button className="mt-6 mb-2 bg-indigo-600 text-white rounded px-12 py-2 text-xl">
-        Details
-      </button>
+      <div className="accordion relative">
+        <Accordion collapsible>
+          <AccordionItem>
+            <AccordionButton className="mt-6 mb-2 bg-indigo-600 text-white rounded px-12 py-2 text-xl active:bg-indigo-800">
+              Details
+            </AccordionButton>
+
+            <AccordionPanel>
+              <p className="absolute bg-white text-gray-800 text-justify p-3 border-2 border-indigo-600 rounded text-lg z-10">
+                {instructorAbout}
+              </p>
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </div>
     </div>
   );
 };

3779bb4d7924cd25cebf3de889b81305c07abb2e
Moeen Mahmud <write2fahim007@gmail.com>
2021-10-04 17:02:30 +0600

    added instructors invitation

--- src/components/Instructors/Instructors.js
+++ src/components/Instructors/Instructors.js
@@ -27,6 +27,18 @@ const Instructors = () => {
           <Instructor instructor={instructor} key={instructor.id}></Instructor>
         ))}
       </div>
+      <div className="flex justify-around items-center mb-16 mt-16">
+        <div className="w-1/2">
+          <h1 className="text-5xl font-bold mb-8">Come teach with us</h1>
+          <p className="text-xl leading-snug">
+            Become an instructor and change lives
+            <br />— including your own
+          </p>
+        </div>
+        <div className="w-1/2">
+          <img src="./images/teach-us.jpg" alt="Teach with us" />
+        </div>
+      </div>
     </div>
   );
 };
